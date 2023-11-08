b2fd1a0ff6d40896ddf004e8702cd2347aaf6b76
Tanvir Rudro <rudro987@gmail.com>
2023-11-08 12:26:58 +0600

    GET api created for fetching all Jobs data

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const multer = require('multer');
+const multer = require("multer");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,7 +12,7 @@ app.use(express.json());
 
 const upload = multer();
 
-const fileUpload = upload.single('image');
+const fileUpload = upload.single("image");
 
 const dbUserName = process.env.DB_USER;
 const dbPassWord = process.env.DB_PASSWORD;
@@ -29,36 +29,44 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const usersCollection = client.db("usersDB").collection("users");
+    const jobsCollection = client.db("usersDB").collection("jobs");
 
-    const usersCollection = client.db('usersDB').collection('users');
-    const jobsCollection = client.db('usersDB').collection('jobs');
+    app.get("/all-jobs", async (req, res) => {
+      try {
+        const cursor = await jobsCollection.find().toArray();
+        console.log(cursor);
+        res.send(cursor);
+      } catch (error) {
+        console.log("error getting all jobdata: ", error.message);
+        res.status(400).send(error.message);
+      }
+    });
 
     app.post("/users", fileUpload, async (req, res) => {
-        const user = req.body;
-        console.log(user);
-        try {
-            const result = await usersCollection.insertOne(user);
+      const user = req.body;
+      console.log(user);
+      try {
+        const result = await usersCollection.insertOne(user);
         res.send(result);
-        } catch (error) {
-            console.log("error posting data: ", error.message);
-            res.status(400).send(error.message);
-        }
-    
-    })
+      } catch (error) {
+        console.log("error posting data: ", error.message);
+        res.status(400).send(error.message);
+      }
+    });
 
     app.post("/add-job", fileUpload, async (req, res) => {
-        const job = req.body;
-        job.applicantsNumber = parseInt(job.applicantsNumber);
-        console.log(job);
-        try {
-            const result = await jobsCollection.insertOne(job);
+      const job = req.body;
+      job.applicantsNumber = parseInt(job.applicantsNumber);
+      console.log(job);
+      try {
+        const result = await jobsCollection.insertOne(job);
         res.send(result);
-        } catch (error) {
-            console.log("error posting data: ", error.message);
-            res.status(400).send(error.message);
-        }
-    
-    })
+      } catch (error) {
+        console.log("error posting data: ", error.message);
+        res.status(400).send(error.message);
+      }
+    });
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
@@ -74,9 +82,8 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
-    res.send("Server running successfully!");
-})
+  res.send("Server running successfully!");
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
